6ad33d518af17188db53a1cae2128129ff6888f0
prasad2210 <57230718+prasad2210@users.noreply.github.com>
2021-06-05 00:30:11 +0530

    day 3

--- js/index.js
+++ js/index.js
@@ -1,14 +1,42 @@
 console.log("sdfsg");
-let nav = document.querySelector('nav');
+let nav = document.querySelector("nav");
 
-window.addEventListener('scroll', function(){
-    if(window.pageYOffset > 100){
-        nav.classList.remove('navbar-dark');
-        nav.classList.add('bg-white', 'shadow', 'navbar-light');
-    }
-    else{
-        nav.classList.remove('bg-white', 'shadow', 'navbar-light');
-        nav.classList.add('navbar-dark');
-
-    }
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 100) {
+    nav.classList.remove("navbar-dark");
+    nav.classList.add("bg-white", "shadow", "navbar-light");
+  } else {
+    nav.classList.remove("bg-white", "shadow", "navbar-light");
+    nav.classList.add("navbar-dark");
+  }
 });
+
+var scrollDistance, distFromTop, windowWidth, horLength;
+calcHeight();
+function calcHeight(){
+
+windowWidth = window.innerWidth;
+horLength = document.querySelector(".element-wrapper").scrollWidth;
+distFromTop = document.querySelector(".horizontal-section").offsetTop;
+scrollDistance = distFromTop + horLength - windowWidth;
+document.querySelector(".horizontal-section").style.height = horLength + "px";
+
+console.log(windowWidth+' '+horLength+ ' '+ distFromTop+' '+scrollDistance);
+
+}
+window.onscroll = function () {
+  var scrollTop = window.pageYOffset;
+
+  if (scrollTop >= distFromTop && scrollTop <= scrollDistance) {
+    document.querySelector(".element-wrapper").style.transform =
+      "translateX(-" + (scrollTop - distFromTop) + "px)";
+  }
+  else if(scrollTop < distFromTop){
+    document.querySelector(".element-wrapper").style.transform =
+    "translateX(" + (0) + "px)";
+  }
+  else if(scrollTop > scrollDistance){
+    document.querySelector(".element-wrapper").style.transform =
+      "translateX(-" + (horLength- windowWidth) + "px)";
+  }
+};
