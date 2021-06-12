b9f5f65d9485d6cb6a764c1f3ec818abc6ffc0cc
prasad2210 <57230718+prasad2210@users.noreply.github.com>
2021-06-12 09:24:32 +0530

    audio add

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ let nav = document.querySelector("nav");
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 130) {
     // nav.classList.remove("navbar-dark");
-    nav.classList.add("bg-white", "shadow");
+    // nav.classList.add("bg-white", "shadow");
     $(".nav-title-div").css("display", 'block');
   } else {
-    nav.classList.remove("bg-white", "shadow");
+    // nav.classList.remove("bg-white", "shadow");
     // nav.classList.add("navbar-dark");
     $(".nav-title-div").css("display", 'none');
   }
@@ -44,3 +44,46 @@ window.onscroll = function () {
 
   }
 };
+
+document.getElementById("playpause1").addEventListener("click", function(){
+	var audio = document.getElementById('track1');
+  if(this.className == 'is-playing'){
+    this.className = "";
+    this.src = './images/play.png';
+    audio.pause();
+  }else{
+    this.className = "is-playing";
+    this.src = './images/pause.png';
+    audio.play();
+  }
+
+});
+
+
+document.getElementById("playpause2").addEventListener("click", function(){
+	var audio = document.getElementById('track2');
+  if(this.className == 'is-playing'){
+    this.className = "";
+    this.src = './images/play.png';
+    audio.pause();
+  }else{
+    this.className = "is-playing";
+    this.src = './images/pause.png';
+    audio.play();
+  }
+
+});
+
+document.getElementById("playpause3").addEventListener("click", function(){
+	var audio = document.getElementById('track3');
+  if(this.className == 'is-playing'){
+    this.className = "";
+    this.src = './images/play.png';
+    audio.pause();
+  }else{
+    this.className = "is-playing";
+    this.src = './images/pause.png';
+    audio.play();
+  }
+
+});
