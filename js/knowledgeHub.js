8de7ec632cb154bfc151aea9d77593e782f54c35
prasad2210 <57230718+prasad2210@users.noreply.github.com>
2021-07-01 16:05:35 +0530

    comics

--- js/knowledgeHub.js
+++ js/knowledgeHub.js
@@ -1,3 +1,5 @@
+new WOW().init();
+
 window.addEventListener("load", function () {
     const loader = document.querySelector(".loader");
     loader.className += " hidden"; // class "loader hidden"
@@ -36,3 +38,5 @@ window.addEventListener("scroll", function () {
     }
   }
 });
+
+
